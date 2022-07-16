c56793a9831416ddbb9e6866b8bb4c4064ceb3e5
Stephane P. Pericat <shortwavaudio@gmail.com>
2022-07-16 13:27:37 -0500

    initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'co.shortwavaudio.ionic-demo',
   appName: 'ionic-demo',
   webDir: 'dist',
   bundledWebRuntime: false
